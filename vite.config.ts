a0c41bc8ab73c1f30c90a0a5ae085d6d9f7cc28d
Gustavo <gumoises@live.com>
2024-11-21 17:48:34 -0300

    added github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gumoises94",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
